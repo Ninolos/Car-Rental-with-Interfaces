8f5e8445b64dc1b33c53e3d1a6d61311d70b4a90
Jonathan Loth <ninoloth@gmail.com>
2021-06-02 21:41:25 -0300

    first implementations without interfaces for example

---
+++ LocacaoInterface/Entities/Invoice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace LocacaoInterface.Entities
+{
+    class Invoice
+    {
+        public double BasicPayments { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayments, double tax)
+        {
+            BasicPayments = basicPayments;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayments + Tax;  }
+        }
+
+        public override string ToString()
+        {
+            return "Basic payment: "
+                + BasicPayments.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotalPayment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture)
+
+        }
+    }
+}
